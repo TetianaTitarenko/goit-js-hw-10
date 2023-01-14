99a18a6fb4bb95fa33a3dab0fe2161d92905b0bd
TetianaTitarenko <110638769+TetianaTitarenko@users.noreply.github.com>
2023-01-14 18:35:17 +0000

    Deploying to gh-pages from @ TetianaTitarenko/goit-js-hw-10@99a6114d9d8d5dfb4c157095343223790ec9fa95 🚀

--- index.aef8b324.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;fetch("".concat("https://restcountries.com/v3.1/name/all","?fields=").concat(n,",capital,population,flags,languages")).then((function(n){return console.log(n)}))}();
-//# sourceMappingURL=index.aef8b324.js.map
--- index.eb67d2ec.js
+++
@@ -1,2 +0,0 @@
-var a;fetch(`https://restcountries.com/v3.1/name/all?fields=${a},capital,population,flags,languages`).then((a=>console.log(a)));
-//# sourceMappingURL=index.eb67d2ec.js.map
